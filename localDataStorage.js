352af792dd1614ee9cb5bf84e543633a38ecbeb2
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-04 02:20:23 +0000

    linter error fixed

--- localDataStorage.js
+++ localDataStorage.js
@@ -3,18 +3,17 @@ const userFullName = document.getElementById('ContactMefullName');
 const userEmailAddress = document.getElementById('contactMeEmail');
 const userMessage = document.getElementById('contactMeMessage');
 
-const userDataObj = {
-  userName: userFullName.value,
-  userEmail: userEmailAddress.value,
-  userMsg: userMessage.value,
-};
-
 contactForm.addEventListener('change', () => {
+  const userDataObj = {
+    userName: userFullName.value,
+    userEmail: userEmailAddress.value,
+    userMsg: userMessage.value,
+  };
   const storeData = JSON.stringify(userDataObj);
   localStorage.setItem('userDetails', storeData);
 });
 
-const newStoraData = localStorage.getItem('userDetails', userDataObj);
+const newStoraData = localStorage.getItem('userDetails');
 const newStoreData = JSON.stringify(newStoraData);
 
 userFullName.value = newStoreData.userName;
