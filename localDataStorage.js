f23f6c79a5e5db352b8471373e02b9443b685bde
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-04 02:13:57 +0000

    fixed linter errors

--- localDataStorage.js
+++ localDataStorage.js
@@ -1,23 +1,22 @@
-const contactForm = document.getElementById("contactMeForm")
-const userFullName = document.getElementById("ContactMefullName");
-const userEmailAddress = document.getElementById("contactMeEmail");
-const userMessage = document.getElementById("contactMeMessage");
+const contactForm = document.getElementById('contactMeForm');
+const userFullName = document.getElementById('ContactMefullName');
+const userEmailAddress = document.getElementById('contactMeEmail');
+const userMessage = document.getElementById('contactMeMessage');
 
-contactForm.addEventListener("change", () => {
-    const userDataObj = {
-        userName: userFullName.value,
-        userEmail: userEmailAddress.value,
-        userMsg: userMessage.value
-    }
+const userDataObj = {
+  userName: userFullName.value,
+  userEmail: userEmailAddress.value,
+  userMsg: userMessage.value,
+};
 
-    const storeData = JSON.stringify(userDataObj);
-    localStorage.setItem("userDetails", storeData);
-})
+contactForm.addEventListener('change', () => {
+  const storeData = JSON.stringify(userDataObj);
+  localStorage.setItem('userDetails', storeData);
+});
 
-const newStoraData = localStorage.getItem("userDetails", userDataObj)
-const newStoreData = JSON.stringify(newStoraData)
+const newStoraData = localStorage.getItem('userDetails', userDataObj);
+const newStoreData = JSON.stringify(newStoraData);
 
-userFullName.value = newStoraData.userName
-userEmailAddress.value = newStoraData.userEmail;
-userMessage.value = newStoraData.userMsg;
-
+userFullName.value = newStoreData.userName;
+userEmailAddress.value = newStoreData.userEmail;
+userMessage.value = newStoreData.userMsg;
